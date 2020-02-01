c6efc14f32e6d73124145b17fcb9174060862493
Lorran Sutter <sutterlorran@gmail.com>
2020-01-31 19:06:31 -0500

    Update how it works style

--- js/general.js
+++ js/general.js
@@ -1,7 +1,19 @@
+var body = document.querySelector("body");
 var burgerIcon = document.querySelector("#burger-icon");
 var navBar = document.querySelector(".nav-bar");
 
 function dropdownMenu() {
+    // console.log("here1");
+    // console.log(event);
+    // console.log(event.target);
+    // if(!event.target.matches("header")){
+    //     console.log("here2");
+    //     if (navBar.classList.contains("nav-bar-dropdown")) {
+    //         navBar.classList.remove("nav-bar-dropdown");
+    //     }
+    //     return;
+    // }
+
     if (navBar.classList.contains("nav-bar-dropdown")) {
         navBar.classList.remove("nav-bar-dropdown");
     } else {
@@ -9,4 +21,5 @@ function dropdownMenu() {
     }
 }
 
+// body.addEventListener("click", dropdownMenu);
 burgerIcon.addEventListener("click", dropdownMenu);
