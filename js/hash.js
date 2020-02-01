d82da48e22c5b4f7549f72aa3bb5aee27ca13d29
Lorran Sutter <sutterlorran@gmail.com>
2020-02-01 03:26:55 -0500

    Update last files before publishing

--- js/hash.js
+++ js/hash.js
@@ -1,4 +1,4 @@
-function RandomHash(size) {
+function randomHash(size) {
 	var result = [];
     var AlphaNumber = "0123456789abcdefghijklmnopqrstuvxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
     for (var i = 0 ; i < size; i++) { 
@@ -6,14 +6,39 @@ function RandomHash(size) {
       result.push(AlphaNumber.charAt(index));
     }
     return(result.join(""));
-   // console.log(result.join(""));
 }
-var size = 32;
-var tRun = 0;
-var interval = setInterval(function(){
-    tRun += 1;
-    if(tRun === 100){
-        clearInterval(interval);
+function sendReceive(size) {
+	var result = [];
+    var AlphaNumber = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz"; 
+    for (var i = 0 ; i < size; i++) { 
+    	var  index = AlphaNumber.length * Math.random();
+      result.push(AlphaNumber.charAt(index));
     }
-    document.getElementById("hash").innerHTML = RandomHash(size);
-},30); 
+    return(result.join(""));
+}
+
+function hashTable() {
+  var size = 20;
+  var table = document.getElementById("table-hash");    
+  for (var i = 0 ; i < 10; i++) { 
+      var row = table.insertRow(0);
+      var dttime = row.insertCell(0);
+      var send = row.insertCell(1);
+      var receive = row.insertCell(2);
+      var s = sendReceive(6);
+      var r = sendReceive(6);
+      send = s;
+      receive = r;
+      var dtObj = new Date();
+      var month = ('0' + (dtObj.getMonth() + 1)).slice(-2);
+      var date = ('0' + dtObj.getDate()).slice(-2);
+      var year = dtObj.getFullYear();
+      var hours =dtObj.getHours();
+      var mins =dtObj.getMinutes() + i;
+      var secs =dtObj.getSeconds();
+      var shortDate = year + '/' + month + '/' + date + " " + hours + ":" + mins + ":" + secs;
+      row.innerHTML = randomHash(size) + shortDate + send + receive;
+  }
+}
+
+hashTable();
