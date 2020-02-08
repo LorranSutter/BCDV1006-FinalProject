adcb2943c3ab373add0505d00e6bdbc2358b094a
Lorran Sutter <sutterlorran@gmail.com>
2020-02-07 23:33:31 -0500

    Fixes to get the same as published

--- js/hash.js
+++ js/hash.js
@@ -17,27 +17,26 @@ function sendReceive(size) {
     return(result.join(""));
 }
 
+function randomDate(startDate, endDate) {
+  return new Date(startDate.getTime() + Math.random() * (startDate.getTime() - endDate.getTime()));
+}
+
 function hashTable() {
   var size = 20;
-  var table = document.getElementById("table-hash");    
+  var table = document.querySelector("#table-realtime-transactions tbody");    
   for (var i = 0 ; i < 10; i++) { 
-      var row = table.insertRow(0);
-      var dttime = row.insertCell(0);
-      var send = row.insertCell(1);
-      var receive = row.insertCell(2);
+      var row = table.insertRow();
+      var hash = row.insertCell(0);
+      var dttime = row.insertCell(1);
+      var send = row.insertCell(2);
+      var receive = row.insertCell(3);
       var s = sendReceive(6);
       var r = sendReceive(6);
-      send = s;
-      receive = r;
-      var dtObj = new Date();
-      var month = ('0' + (dtObj.getMonth() + 1)).slice(-2);
-      var date = ('0' + dtObj.getDate()).slice(-2);
-      var year = dtObj.getFullYear();
-      var hours =dtObj.getHours();
-      var mins =dtObj.getMinutes() + i;
-      var secs =dtObj.getSeconds();
-      var shortDate = year + '/' + month + '/' + date + " " + hours + ":" + mins + ":" + secs;
-      row.innerHTML = randomHash(size) + shortDate + send + receive;
+
+      hash.innerHTML = randomHash(size);
+      dttime.innerHTML = randomDate(new Date(2020,01,01), new Date).getTime();
+      send.innerHTML = s;
+      receive.innerHTML = r;
   }
 }
 
