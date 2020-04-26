5d291ced35a601102cec525679ea3338f1b79bf0
Lorran Sutter <sutterlorran@gmail.com>
2020-04-26 17:00:50 -0400

    Remove unecessary comments

--- js/general.js
+++ js/general.js
@@ -3,17 +3,6 @@ var burgerIcon = document.querySelector("#burger-icon");
 var navBar = document.querySelector(".nav-bar");
 
 function dropdownMenu() {
-    // console.log("here1");
-    // console.log(event);
-    // console.log(event.target);
-    // if(!event.target.matches("header")){
-    //     console.log("here2");
-    //     if (navBar.classList.contains("nav-bar-dropdown")) {
-    //         navBar.classList.remove("nav-bar-dropdown");
-    //     }
-    //     return;
-    // }
-
     if (navBar.classList.contains("nav-bar-dropdown")) {
         navBar.classList.remove("nav-bar-dropdown");
     } else {
@@ -21,5 +10,4 @@ function dropdownMenu() {
     }
 }
 
-// body.addEventListener("click", dropdownMenu);
 burgerIcon.addEventListener("click", dropdownMenu);
